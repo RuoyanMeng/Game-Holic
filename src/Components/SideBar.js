440d97b6d8be1e2bdebcc54fc54737b0d1e59787
Dariald <39576951+Dariald@users.noreply.github.com>
2019-03-29 12:44:42 +0100

    format fixed

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -5,28 +5,26 @@ import "../Styles/sidebar.css";
 
 class SideBar extends Component {
 
-    render() {
-        return ( <
-            div className = 'sidebar' > {
-                /* <h1>
-                          this is SideBar
-                        </h1> */
-            } <
-            div className = 'sidebar-list' > { /* <Icon type="home" theme="twoTone" /> */ } <
-            Link to = "/" > < span > H < /span>ome </Link > < br / > < br / >
-            <
-            Link to = "/Hottest" > H < span > o < /span>ttest < /Link > < br / > < br / >
-            <
-            Link to = "/Latest" > < span > L < /span>at<span>e</span > st < /Link><br / > < br / > { /* <Icon type="star" theme="twoTone" /> */ } <
-            Link to = "/WishList" > W < span > i < /span>shList < /Link > < br / > {
-                /* <Link to="/WishList"> Playing</Link><br/>
-                   <Link to="/WishList"> Plan to play</Link><br/>
-                   <Link to="/WishList"> Played</Link><br/> */
-            } <
-            /div> < /
-            div >
-        )
-    }
+  render() {
+    return (
+      <div className='sidebar' > {
+        /* <h1>
+                  this is SideBar
+                </h1> */
+      }
+        <div className='sidebar-list' > { /* <Icon type="home" theme="twoTone" /> */}
+          <Link to="/"> <span> H </span>ome </Link > <br /> <br />
+          <Link to="/Hottest" > H <span> o </span>ttest </Link > <br /> <br />
+          <Link to="/Latest" > <span> L </span>at<span>e</span > st </Link><br /> <br /> { /* <Icon type="star" theme="twoTone" /> */}
+          <Link to="/WishList" > W <span> i </span>shList </Link> <br /> {
+            /* <Link to="/WishList"> Playing</Link><br/>
+               <Link to="/WishList"> Plan to play</Link><br/>
+               <Link to="/WishList"> Played</Link><br/> */
+          }
+        </div>
+      </div >
+    )
+  }
 };
 
 export default SideBar;
--- src/Components/SingleGame.js
+++ src/Components/SingleGame.js
@@ -14,8 +14,8 @@ class GameGrid extends Component {
     static propTypes = {
         game: PropTypes.object.isRequired,
         isFetching: PropTypes.string.isRequired
-            // Injected by React Router
-            //children: PropTypes.node
+        // Injected by React Router
+        //children: PropTypes.node
     };
 
     constructor(props) {
@@ -44,117 +44,96 @@ class GameGrid extends Component {
         let gameDetails = null;
         switch (this.props.isFetching) {
             case "LOADING":
-                gameDetails = < em > Loading... < /em>;
+                gameDetails = <em> Loading... </em>;
                 break;
             case "LOADED":
                 //edit all the elements and layout of gamedetails card here
-                gameDetails = ( <
-                    div className = "game-detail-card" >
-                    <
-                    Row >
-                    <
-                    Col span = { 16 } >
-                    <
-                    div className = "game-summary" >
-                    <
-                    h2 > Summary: < /h2> {
-                        this.props.game.summary && < p > { this.props.game.summary } < /p>} <
-                            /div> <
-                            /Col> <
-                            Col span = { 8 } >
-                            <
-                            h2 > RATE: < /h2> {
-                                this.props.game.rating && ( <
-                                    p className = "game-popularity" > { this.props.game.rating.toFixed(2) } <
-                                    /p>
-                                )
-                            } <
-                            /Col> <
-                            /Row> <
-                            /div>
-                    );
-                    break;
-                    default:
-                    gameDetails = < b > Failed to load data, please
-                    try again < /b>;
-                    break;
-                }
-
-                return ( <
-                    div >
-                    <
-                    Row >
-                    <
-                    Col span = { 4 } >
-                    <
-                    SideBar / >
-                    <
-                    /Col>
-
-                    <
-                    Col span = { 20 } >
-                    <
-                    div className = "main-wrap" >
-                    <
-                    Link to = "/"
-                    className = "back-to" >
-                    Back to Search <
-                    /Link> <
-                    div className = "game-content" > { /* <h1 className="single-game-title">this is game: {this.state.currentId}</h1> */ } {
-                        this.props.game.name && ( <
-                            h1 className = "single-game-title" > { this.props.game.name } < /h1>
-                        )
-                    } <
-                    Row >
-                    <
-                    Col span = { 4 } > {
-                        this.props.game.cover && ( <
-                            img className = "game-cover"
-                            src = { "https:" + this.props.game.cover.url }
-                            alt = { this.props.game.name }
-                            />
-                        )
-                    } <
-                    /Col> <
-                    Col span = { 20 } > { gameDetails } < /Col> <
-                    /Row> {
-                        this.props.game.name && ( <
-                            Button type = "primary"
-                            onClick = {
-                                () => this.addWishListClick() } >
-                            Add to Wishlist <
-                            /Button>
-                        )
-                    } <
-                    /div> <
-                    /div>
-
-                    <
-                    /Col> <
-                    /Row> <
-                    /div>
+                gameDetails = (
+                <div className="game-detail-card">
+                    <Row>
+                        <Col span={16} >
+                            <div className="game-summary" >
+                                <h2> Summary: </h2> 
+                                {this.props.game.summary && <p> {this.props.game.summary}</p>} 
+                        </div> 
+                        </Col> 
+                        <Col span={8} >
+                                <h2> RATE: </h2> 
+                                {this.props.game.rating && (
+                                    <p className="game-popularity"> 
+                                        {this.props.game.rating.toFixed(2)} 
+                                    </p>
+                            )} 
+                        </Col> 
+                    </Row> 
+                 </div>
                 );
+                break;
+            default:
+                gameDetails = <b> Failed to load data, please try again </b>;
+                break;
         }
-    }
+
+        return (
+        <div>
+            <Row>
+                <Col span={4}>
+                    <SideBar/>
+                </Col>
+
+                <Col span={20}>
+                    <div className="main-wrap" >
+                        <Link to="/"  className="back-to" >
+                            Back to Search 
+                         </Link> 
+                        <div className="game-content">
+                            {this.props.game.name && (
+                                <h1 className="single-game-title">{this.props.game.name}</h1>
+                            )} 
+                            <Row>
+                                <Col span={4} > {
+                                     this.props.game.cover && (
+                                        <img 
+                                            className="game-cover"
+                                            src={"https:" + this.props.game.cover.url}
+                                            alt={this.props.game.name}
+                                        />
+                                    )} 
+                                </Col>
+                                <Col span={20}> {gameDetails} </Col>
+                            </Row> 
+                            {this.props.game.name && (
+                                <Button type="primary"  onClick={() => this.addWishListClick()}>
+                                    Add to Wishlist 
+                                </Button>
+                            )} 
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+         </div>
+    );
+   }
+}
 
     const mapStateToProps = state => {
         //console.log(state.singleGame)
         //console.log(state.singleGame.isFetching)
         //console.log(state.wishlist)
         return {
-            //wishlist:state.wishlist,
-            isFetching: state.singleGame.isFetching,
-            game: state.singleGame.game
+                    //wishlist:state.wishlist,
+                    isFetching: state.singleGame.isFetching,
+                game: state.singleGame.game
+            };
         };
-    };
-
+    
     const mapDispatchToProps = dispatch => {
         return {
-            actions: bindActionCreators(actions, dispatch)
+                    actions: bindActionCreators(actions, dispatch)
+            };
         };
-    };
-
-    export default connect(
-        mapStateToProps,
-        mapDispatchToProps
+    
+        export default connect(
+            mapStateToProps,
+            mapDispatchToProps
     )(GameGrid);
