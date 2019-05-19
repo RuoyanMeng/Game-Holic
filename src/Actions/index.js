6c49383709120cac376d72525fdab8fc91aec2f4
Ruoyan Meng <ruoyanmeng@gmail.com>
2019-05-19 20:49:07 +0200

    comments bugs fixed

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,174 +1,203 @@
-import gamesApi from '../Api/api'
-import * as types from './actionTypes'
-
+import gamesApi from "../Api/api";
+import * as types from "./actionTypes";
 
 // the currently visible error message.
 export const errorMessage = () => ({
-    type: types.ERROR_MESSAGE
-})
+  type: types.ERROR_MESSAGE
+});
 
 //load index results action
 export function loadGameSuccess(games) {
-    return {
-        type: types.LOAD_GAMES_SUCCESS, games
-    };
+  return {
+    type: types.LOAD_GAMES_SUCCESS,
+    games
+  };
 }
 
 export function getAllGames(inputValue) {
-    return function (dispatch) {
-        gamesApi.getAllGames(inputValue).then(games => {
-            dispatch(loadGameSuccess(games));
-        }).catch(error => {
-            dispatch(errorMessage)
-            throw (error);
-        });
-    }
+  return function(dispatch) {
+    gamesApi
+      .getAllGames(inputValue)
+      .then(games => {
+        dispatch(loadGameSuccess(games));
+      })
+      .catch(error => {
+        dispatch(errorMessage);
+        throw error;
+      });
+  };
 }
 
 //load index results action
 export function loadSearchResultsSuccess(searchResults) {
-    return {
-        type: types.LOAD_SEARCH_RESULTS_SUCCESS, searchResults
-    };
+  return {
+    type: types.LOAD_SEARCH_RESULTS_SUCCESS,
+    searchResults
+  };
 }
 
 export function getSearchResults(inputValue) {
-    return function (dispatch) {
-        gamesApi.getSearchResults(inputValue).then(searchResults => {
-            dispatch(loadSearchResultsSuccess(searchResults));
-        }).catch(error => {
-            dispatch(errorMessage)
-            throw (error);
-        });
-    }
+  return function(dispatch) {
+    gamesApi
+      .getSearchResults(inputValue)
+      .then(searchResults => {
+        dispatch(loadSearchResultsSuccess(searchResults));
+      })
+      .catch(error => {
+        dispatch(errorMessage);
+        throw error;
+      });
+  };
 }
 
-
-
 export function loadSingleGameSuccess(game) {
-    return {
-        type: types.LOAD_SINGLEGAME_SUCCESS, game
-    };
+  return {
+    type: types.LOAD_SINGLEGAME_SUCCESS,
+    game
+  };
 }
 
 export function getSingleGame(id) {
-    return (dispatch) => {
-        gamesApi.getGame(id).then(game => {
-            dispatch(loadSingleGameSuccess(game));
-        }).catch(error => {
-            dispatch(errorMessage);
-            throw (error);
-        });
-    }
+  return dispatch => {
+    gamesApi
+      .getGame(id)
+      .then(game => {
+        dispatch(loadSingleGameSuccess(game));
+      })
+      .catch(error => {
+        dispatch(errorMessage);
+        throw error;
+      });
+  };
 }
 
-export const addItemToList = (briefGameInfo) => {
-    let uid = briefGameInfo.uid;
-    let gameId = briefGameInfo.gameID;
-    let playStatus = briefGameInfo.playStatus;
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
+export const addItemToList = briefGameInfo => {
+  let uid = briefGameInfo.uid;
+  let gameId = briefGameInfo.gameID;
+  let playStatus = briefGameInfo.playStatus;
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
 
-        //add new item to correspend list
-        firestore.collection('users').doc(uid).collection('games').doc(gameId).set({
-            ...briefGameInfo
-        }).then(() => {
-            dispatch({ type: types.ADD_LIST_SUCCESS, uid });
-            dispatch({ type: types.GET_PLAYSTATUS_SUCCESS, playStatus })
-        }).catch(err => {
-            dispatch({ type: types.ADD_LIST_ERROR }, err);
-        });
-    }
-}
-
+    //add new item to correspend list
+    firestore
+      .collection("users")
+      .doc(uid)
+      .collection("games")
+      .doc(gameId)
+      .set({
+        ...briefGameInfo
+      })
+      .then(() => {
+        dispatch({ type: types.ADD_LIST_SUCCESS, uid });
+        dispatch({ type: types.GET_PLAYSTATUS_SUCCESS, playStatus });
+      })
+      .catch(err => {
+        dispatch({ type: types.ADD_LIST_ERROR }, err);
+      });
+  };
+};
 
 export function getPlayStatus(id) {
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
-        var playStatus = null;
-        if (id.uid === null) {
-            playStatus = 'Set Status'
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    var playStatus = null;
+    if (id.uid === null) {
+      playStatus = "Set Status";
+      dispatch({ type: "GET_PLAYSTATUS_SUCCESS", playStatus });
+    } else {
+      firestore
+        .collection("users")
+        .doc(id.uid)
+        .collection("games")
+        .where("gameID", "==", id.gameID)
+        .get()
+        .then(games => {
+          if (games.size === 0) {
+            playStatus = "Set Status";
             dispatch({ type: "GET_PLAYSTATUS_SUCCESS", playStatus });
-        } else {
-            firestore.collection('users').doc(id.uid).collection('games').where('gameID', '==', id.gameID).get().then(games => {
-                if (games.size === 0) {
-                    playStatus = 'Set Status'
-                    dispatch({ type: "GET_PLAYSTATUS_SUCCESS", playStatus });
-                } else {
-                    games.forEach(game => {
-                        playStatus = game.data().playStatus
-                        dispatch({ type: "GET_PLAYSTATUS_SUCCESS", playStatus });
-                    })
-                }
-            }).catch(err => {
+          } else {
+            games.forEach(game => {
+              playStatus = game.data().playStatus;
+              dispatch({ type: "GET_PLAYSTATUS_SUCCESS", playStatus });
             });
-        }
-
+          }
+        })
+        .catch(err => {});
     }
+  };
 }
 
 export function getGameList(listType) {
-
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
-        var gameList = [];
-        firestore.collection('users').doc(listType.uid).collection('games').where('playStatus', '==', listType.listType).get().then(games => {
-            if (games.size === 0) {
-                gameList = {}
-                dispatch({ type: "GET_" + listType.listType + "_SUCCESS", gameList });
-            } else {
-                games.forEach(game => {
-                    gameList.push(game.data());
-                })
-                dispatch({ type: "GET_" + listType.listType + "_SUCCESS", gameList });
-            }
-        }).catch(err => {
-            dispatch({ type: 'GET_PLAYSTATUS_ERROR' }, err);
-        });
-    }
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    var gameList = [];
+    firestore
+      .collection("users")
+      .doc(listType.uid)
+      .collection("games")
+      .where("playStatus", "==", listType.listType)
+      .get()
+      .then(games => {
+        if (games.size === 0) {
+          gameList = {};
+          dispatch({ type: "GET_" + listType.listType + "_SUCCESS", gameList });
+        } else {
+          games.forEach(game => {
+            gameList.push(game.data());
+          });
+          dispatch({ type: "GET_" + listType.listType + "_SUCCESS", gameList });
+        }
+      })
+      .catch(err => {
+        dispatch({ type: "GET_PLAYSTATUS_ERROR" }, err);
+      });
+  };
 }
 
-export function resetState(section){
-    return (dispatch) => {
-        dispatch({ type: types.RESET_STATE+'_'+section });
-    }
+export function addComments(commentInfo) {
+  let commentList = commentInfo.commentList;
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("commentList")
+      .doc(commentInfo.gameID)
+      .set({
+        ...JSON.parse(JSON.stringify(commentList))
+      })
+      .then(() => {
+        dispatch({ type: types.ADD_COMMENTS_SUCCESS });
+      })
+      .catch(err => {
+        dispatch({ type: types.ADD_COMMENTS_ERROR }, err);
+      });
+  };
 }
 
-
-export function addComments(commentInfo){
-    let commentList = commentInfo.commentList
-    return(dispatch, getState, { getFirestore })=>{
-        const firestore = getFirestore();
-        firestore.collection('commentList').doc(commentInfo.gameID).set({
-            ...JSON.parse(JSON.stringify(commentList))
-        }).then(() => {
-                dispatch({ type: types.ADD_COMMENTS_SUCCESS});
-            }).catch(err => {
-                dispatch({ type: types.ADD_COMMENTS_ERROR }, err);
-            });
-    }
+export function getComments(gameID) {
+  let commentList;
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("commentList")
+      .doc(gameID)
+      .get()
+      .then(comments => {
+        console.log(comments._document)
+        if (comments._document === null) {
+          commentList = [];
+        } else {
+          commentList = Object.values(comments.data());
+        }
+        dispatch({ type: types.GET_COMMENTS_SUCCESS, commentList });
+      })
+      .catch(err => {
+        dispatch({ type: types.GET_COMMENTS_ERROR }, err);
+      });
+  };
 }
 
-export function getComments(gameID){
-    let commentList;
-    return(dispatch, getState, { getFirestore })=>{
-        const firestore = getFirestore();
-        firestore.collection('commentList').doc(gameID).get().then(comments => {
-                
-                commentList = Object.values(comments.data());
-                
-                dispatch({ type: types.GET_COMMENTS_SUCCESS, commentList});
-            }).catch(err => {
-                dispatch({ type: types.GET_COMMENTS_ERROR }, err);
-            });
-    }
+export function resetState(section) {
+  return dispatch => {
+    dispatch({ type: types.RESET_STATE + "_" + section });
+  };
 }
-
-
-
-
-
-
-
-
-
--- src/Components/Containers/SingleGame.js
+++ src/Components/Containers/SingleGame.js
@@ -6,9 +6,9 @@ import { bindActionCreators } from "redux";
 
 import * as actions from "../../Actions/index";
 import { signIn } from "../../Actions/authActions";
-import GameDetails from '../Presentational/gameDetails'
+import GameDetails from "../Presentational/gameDetails";
 import Header from "../Header";
-import Comments from '../Presentational/Comments'
+import Comments from "../Presentational/Comments";
 
 import "../../Styles/singlegame.scss";
 import { Icon } from "antd";
@@ -27,7 +27,7 @@ class SingleGame extends Component {
     super(props);
     this.state = {
       currentId: this.props.match.params.id,
-      playStatus: this.props.playStatus,
+      playStatus: this.props.playStatus
     };
   }
   componentDidMount() {
@@ -37,14 +37,10 @@ class SingleGame extends Component {
   }
 
   componentWillUnmount = () => {
-    this.props.actions.resetState('SINGLEGAME');
-  }
-
-
-
+    this.props.actions.resetState("SINGLEGAME");
+  };
 
   render() {
-
     const {
       game,
       auth,
@@ -62,22 +58,26 @@ class SingleGame extends Component {
 
     switch (isFetching) {
       case "LOADING":
-        gameDetails = <h1 className='white mt7 ml7'><Icon type="loading" /></h1>;
+        gameDetails = (
+          <h1 className="white mt7 ml7">
+            <Icon type="loading" />
+          </h1>
+        );
         break;
       case "LOADED":
         if (auth.isEmpty) {
           let signUp = <Link to="/SignUp">Sign Up</Link>;
-          gameDetails = <GameDetails
-            game={game}
-            history={this.props.history}
-            auth={auth}
-            playStatus={playStatus}
-            authError={authError}
-            signIn={this.props.signIn}
-            signUp={signUp}
-          >
-          </GameDetails>
-
+          gameDetails = (
+            <GameDetails
+              game={game}
+              history={this.props.history}
+              auth={auth}
+              playStatus={playStatus}
+              authError={authError}
+              signIn={this.props.signIn}
+              signUp={signUp}
+            />
+          );
         } else {
           if (!this.props.auth.isEmpty) {
             let id = {
@@ -91,14 +91,16 @@ class SingleGame extends Component {
               gameDetails = <em>Loading...</em>;
               break;
             case "GET_PLAYSTATUS_SUCCESS":
-              gameDetails = <GameDetails
-                game={game}
-                history={this.props.history}
-                actions={this.props.actions}
-                auth={auth}
-                playStatus={playStatus}
-                gameID={this.state.currentId}
-              />
+              gameDetails = (
+                <GameDetails
+                  game={game}
+                  history={this.props.history}
+                  actions={this.props.actions}
+                  auth={auth}
+                  playStatus={playStatus}
+                  gameID={this.state.currentId}
+                />
+              );
               break;
             default:
               break;
@@ -114,15 +116,25 @@ class SingleGame extends Component {
     let commentDiv = null;
     switch (isFetchingComment) {
       case "LOADING":
-        commentDiv = <h1 className='white mt7 ml7'><Icon type="loading" /></h1>;
+        commentDiv = (
+          <h1 className="white mt7 ml7">
+            <Icon type="loading" />
+          </h1>
+        );
         break;
       case "LOADED":
         console.log(commentList);
-        commentDiv = <Comments actions={actions} gameID={this.state.currentId} commentList={commentList} profile={profile}/>
+        commentDiv = (
+          <Comments
+            actions={actions}
+            gameID={this.state.currentId}
+            commentList={commentList}
+            profile={profile}
+          />
+        );
         break;
       default:
-        commentDiv = <b>Failed to load data, please try again</b>
-
+        commentDiv = <b>Failed to load data, please try again</b>;
     }
 
     return (
@@ -136,7 +148,7 @@ class SingleGame extends Component {
 }
 
 const mapStateToProps = state => {
-  console.log(state.firebase.profile)
+  console.log(state.commentList.isFetchingComment);
   return {
     playStatus: state.singleGame.playStatus,
     isFetching: state.singleGame.isFetching,
